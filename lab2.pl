e9276927ae96234e4f0be4cd63892ad2cef46820
Jonas Dåderman <Jonasdad@kth.se>
2023-11-12 18:38:20 +0100

    andel1&2 working

--- lab2.pl
+++ lab2.pl
@@ -1,26 +1,40 @@
 %indata i format ([Premiss],Slutsats,[[radnr,sats,regel]]).
 %goal 1 test([p],p,[[1,p,premiss]]).
 %goal 2, OR elimination verify(and(a,b),b,).
-
+:-discontiguous(validproof/4).
 verify(InputFileName) :- see(InputFileName), read(Prems), read(Goal), read(Proof), seen, valid_proof(Prems, Goal, Proof).
 
-%validproof([Prem|Rest],Goal,[[,X,premise]|Next]):- member(X,Prems), valid_proof(Rest,Goal, Next). %check if premise is in prems
-%valid_proof(Prems, Goal, [[Proof]|[]]):- nth0(1,last(Proof,Goal),Goal). %check if last element in proof is goal
 getLast([A],A).
 getLast([_|Tail], A):-getLast(Tail,A).
 
-find(Expr, [Expr]).
-find(Expr, [Head|Tail]):-find(Expr, Tail).
+find(X, [X|_]).
+find(X, [_|Tail]):-find(X,Tail).
 
-valid_proof(Prems, Goal, Proof):- getLast(Proof, Last), nth0(1,Last,Goal), validproof(Prems, Goal, Proof, Proof).
-validproof(Prems, Goal,Proof, []).
+valid_proof(Prems, Goal, Proof):- getLast(Proof, Last), nth0(1,Last,Goal),!, validproof(Prems, Goal, Proof, List).
+validproof(Prems, Goal,[], List):-!.
 
 %PREMISE
-validproof(Prems, Goal, Proof,[[_,X,premise]|Next]):-!, find(X,Prems), write('added premise'), validproof(Prems, Goal, Proof,Next).
+validproof(Prems, Goal, [[Num,X,premise]|Next], List):- find(X,Prems), 
+                                                        append(List, [[Num,X,premise]], NewList),!,
+                                                        write(NewList),nl,
+                                                        write('Trying to call validproof again'),nl,
+                                                        write('Prems is: '),write(Prems),nl,
+                                                        write('Goal is: '),write(Goal),nl,
+                                                        write('NewList is: '),write(NewList),nl,
+                                                        write('Next is: '),write(Next),nl,
+                                                        write('validproof('),write(Prems),write(','),write(Goal),write(','),write(Next),write(','),write(NewList),write(')'),nl,
+                                                        validproof(Prems, Goal, Next, NewList).
 
 %ANDEL1
-validproof(Prems, Goal,Proof, [[_,X,andel1(Num)|Next]]):- !,find((Num, (and(X,_)), _), Proof) ,validproof(Prems, Goal, Proof,Next).
-
+validproof(Prems,Goal,[[LineNr,X,andel1(Num)]|Next],List):-find([Num, and(X,_), _], List), 
+                                                        append(List, [[LineNr, X, andel1(Num)]], NewList),!,
+                                                        write(NewList),nl,
+                                                        validproof(Prems, Goal, Next, NewList).
+%ANDEL2
+validproof(Prems,Goal,[[LineNr,X,andel2(Num)]|Next],List):-find([Num, and(_,X), _], List), 
+                                                        append(List, [[LineNr, X, andel2(Num)]], NewList),!,
+                                                        write(NewList),nl,
+                                                        validproof(Prems, Goal, Next, NewList).
 %OREL
 
 %IMPEL
